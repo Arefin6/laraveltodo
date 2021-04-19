cc797ff4e9339d71cd35462a6a0fbd8706b2d614
Arefi6 <arefinhossain3@gmail.com>
2021-04-20 00:26:36 +0600

    set up done

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Todo App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color:#037bfc;
+                color: #fff;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+
+            .title {
+               margin-top:10rem;
+               font-size:60px;
+               text-align: center;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+          <h2 class="title">Todo App</h2>
+        </div>
+    </body>
+</html>
